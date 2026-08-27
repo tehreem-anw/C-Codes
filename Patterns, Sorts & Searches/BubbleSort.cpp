2dde620a150d891be41939b5e8bf69be82db8eba
Tehreem Anwar <tehreemhb@gmail.com>
2026-08-27 13:48:57 -0400

    Update BubbleSort.cpp

--- Patterns, Sorts & Searches/BubbleSort.cpp
+++ Patterns, Sorts & Searches/BubbleSort.cpp
@@ -1,1 +1,26 @@
+#include<iostream>
+using namespace std;
 
+void bubbleSort(int arr[], int n){
+    for(int i = 0; i < n; i++){
+        int isSwap = false;
+        for(int j = 0; j < n - i - 1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+                isSwap = true;
+            }
+        }
+    }
+}
+
+int main(){
+    int arr[7] = {4, 2, 1, 8, 9, 6, 7};
+    int n = 7;
+    bubbleSort(arr, n);
+    cout << "{";
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << "}" << endl;
+    return 0;
+}
