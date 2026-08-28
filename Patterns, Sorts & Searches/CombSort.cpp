3c1769990312bf9127ee4efcb41842f07c7f695f
Tehreem Anwar <tehreemhb@gmail.com>
2026-08-28 14:04:18 -0400

    Update CombSort.cpp

--- Patterns, Sorts & Searches/CombSort.cpp
+++ Patterns, Sorts & Searches/CombSort.cpp
@@ -3,4 +3,9 @@ using namespace std;
 void combSort(int arr[], int n){
   int gap = n;
   float shrink = 1.3;
+  bool swapped = true;
+  while(gap != 1 || swapped == true){
+    gap = (gap/shrink < 1.0) ? 1 : gap/shrink;
+    swapped = false;
+  }
 }
