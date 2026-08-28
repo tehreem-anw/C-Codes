265043948d3d7472eb91564675a0c74d1f4aa0fb
Tehreem Anwar <tehreemhb@gmail.com>
2026-08-28 14:09:07 -0400

    Update CombSort.cpp

--- Patterns, Sorts & Searches/CombSort.cpp
+++ Patterns, Sorts & Searches/CombSort.cpp
@@ -19,7 +19,7 @@ void combSort(int arr[], int n){
 int main(){
     int arr[7] = {4, 2, 1, 8, 9, 6, 7};
     int n = 7;
-    bubbleSort(arr, n);
+    combSort(arr, n);
     cout << "{";
     for(int i = 0; i < n; i++){
         cout << arr[i] << " ";
